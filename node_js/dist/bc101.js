5a5925df493ceb3ef65eae65b9ca1456171821ba
mrmidaw <mr.midaw@gmail.com>
2023-10-05 10:09:19 +0300

    update blockchain code

--- node_js/dist/bc101.js
+++ node_js/dist/bc101.js
@@ -11,18 +11,14 @@ class Block {
     }
     calculateHash() {
         const data = this.index + this.previousHash + this.timestamp + this.data;
-        return crypto
-            .createHash('sha256')
-            .update(data)
-            .digest('hex');
+        return crypto.createHash("sha256").update(data).digest("hex");
     }
 }
-;
 class Blockchain {
     constructor() {
         this.chain = [];
         // Create the genesis block.
-        this.chain.push(new Block(0, '0', Date.now(), 'Genesis block'));
+        this.chain.push(new Block(0, "0", Date.now(), "Genesis block"));
     }
     get latestBlock() {
         return this.chain[this.chain.length - 1];
@@ -32,10 +28,10 @@ class Blockchain {
         this.chain.push(block);
     }
 }
-console.log('Creating the blockchain with the genesis block...');
+console.log("Creating the blockchain with the genesis block...");
 const blockchain = new Blockchain();
-console.log('Mining block #1...');
-blockchain.addBlock('First block');
-console.log('Mining block #2...');
-blockchain.addBlock('Second block');
-console.log(JSON.stringify(blockchain, null, 2));
+console.log("Mining block #1...");
+blockchain.addBlock("First block");
+console.log("Mining block #2...");
+blockchain.addBlock("Second block");
+console.log(JSON.stringify(blockchain, null, 5));
